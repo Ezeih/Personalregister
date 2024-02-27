c8822bf0f945a0c6a2d26c681a7544575b746923
Shubaki <Shubaki@SHUBAKI>
2024-02-27 16:22:27 +0100

    Add project files.

---
+++ Personalregister/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class Personalregister
+{
+    public string Namn { get; set; }
+    public double Lon { get; set; }
+}
+
+
+class Program
+{
+    static void Main()
+    {
+        Personalregister personalregister = new Personalregister();
+        Console.WriteLine("Skriv namn:");
+        personalregister.Namn = Console.ReadLine();
+
+        Console.WriteLine("Skriv lön:");
+        //Conversation till 
+        personalregister.Lon = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Personal " + personalregister.Namn + " har lön " + personalregister.Lon + "Kr.");
+
+        Console.ReadLine();
+
+    }
+}
+
+
